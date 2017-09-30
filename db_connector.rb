9547b03b409877b77b3bf3c69d178e2cabf30c59
David Frederick <warder57@gmail.com>
2017-09-30 13:20:23 -0400

    initial db connection

---
+++ db_connector.rb
@@ -0,0 +1,32 @@
+require 'pg'
+
+class Query
+  attr_accessor :table
+  attr_accessor :query
+
+  def initialize(table, params)
+    self.table = table
+    self.query = build_query(params)
+  end
+
+  def self.execute
+    conn = PG.connect( dbname: 'sales' )
+    conn.exec(self.query) do |result|
+      puts result
+    end
+  end
+end
+
+class Insert < Query
+  def build_query(post)
+    "INSERT INTO #{ self.table } (#{ post.keys.join(',') })
+     VALUES (#{ post.values.join(',') });"
+  end
+end
+
+class BulkInsert < Query
+  def build_query(posts)
+    "INSERT INTO #{ self.table } (#{ posts[0].keys.join(',') })
+     VALUES #{ posts.map { |post| "(#{ post.values.join(',') })" }.join(',') };"
+  end
+end
